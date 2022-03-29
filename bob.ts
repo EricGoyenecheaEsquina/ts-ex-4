59b5faf40f3bb76e0f49811e9cfbf66ce11d3818
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-29 08:59:28 +0200

    Update bob.ts

--- bob.ts
+++ bob.ts
@@ -1,3 +1,16 @@
 export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+  console.log(message);
+  let msg:string=(message as string).trim();
+  let lastChar:string = msg[msg.length - 1]
+  if(lastChar=='?' && msg.toUpperCase() === msg&& msg.toLowerCase() !== msg){
+    return('Calm down, I know what I\'m doing!');
+  }else if(lastChar=='?'){
+    return('Sure.');
+  }else if(msg.toUpperCase() === msg && msg.toLowerCase() !== msg){
+    return('Whoa, chill out!');
+  }else if(msg==""){
+    return('Fine. Be that way!');
+  }else{
+    return('Whatever.')
+  }
 }
